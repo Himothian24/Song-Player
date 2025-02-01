aad4825970ecf94bdb9a1cef68f47609b1d228c9
Himothian24 <dsheth24@mail.com>
2025-01-31 22:28:32 -0500

    button

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pygame
-import sys
 
 pygame.init()
 pygame.mixer.init()
 
-song1 = pygame.mixer.Sound("music/Raving Energy (faster).mp3")
-song_2 = pygame.mixer.Sound("music/Deep and Dirty.mp3")
+song1 = "music/Raving Energy (faster).mp3"
+song_2 = "music/Deep and Dirty.mp3"
+
 screen = pygame.display.set_mode((800,600))
 
 pygame.display.set_caption("Test game")
@@ -16,25 +16,28 @@ text_rect = text.get_rect()
 text_rect.center = (screen.get_width() // 2, 50)
 
 class Button():
-    def __init__(self, x, y, width, height, text):
+    def __init__(self, x, y, width, height, song):
+        size = 23.5
+        if len(song) > size:
+            size = 17
         self.rect = pygame.Rect(x, y, width, height)
-        self.text = text
-        self.font = pygame.font.Font(None, 36)
+        self.text = song
+        self.font = pygame.font.Font(None, size)
         self.text_surf = self.font.render(self.text, True, (255, 255, 255))
         self.text_rect = self.text_surf.get_rect(center=self.rect.center)
+        self.song = pygame.mixer.Sound(song)
 
     def draw(self, screen):
         pygame.draw.rect(screen, (0, 0, 0), self.rect)
         screen.blit(self.text_surf, self.text_rect)
 
-    def is_clicked(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if self.rect.collidepoint(event.pos):
-                return True
-            return False
+    def is_clicked(self):
+        if self.rect.collidepoint(pygame.mouse.get_pos()):
+            pygame.mixer.pause()
+            self.song.play()
+        return False
 
-song = Button(300, 400, 200, 50, "Song 2")
-song2 = Button(50, 400, 200, 50, "Song")
+button_list = [Button(50, 400, 200, 50, song1), Button(350, 400, 200, 50, song_2)]
 
 while True:
     screen.fill((255,0,0))
@@ -42,16 +45,19 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
-        if song.is_clicked(event):
-            pygame.mixer.pause()
-            song1.play()
-        if song2.is_clicked(event):
-            pygame.mixer.pause()
-            song_2.play()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for thing in button_list:
+                thing.is_clicked()
+    
+    for thing in button_list:
+        x = 50
+        thing.x = x
+        thing.draw(screen)
+        x = x + 350
 
     screen.blit(text, text_rect)
-    song2.draw(screen)
-    song.draw(screen)
+    #song2.draw(screen)
+    #song.draw(screen)
 
     
     
